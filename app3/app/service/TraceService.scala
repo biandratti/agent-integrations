1e1a4f0a0b4d416d91cde61e9b33476a5c2d4584
biandratti <maxibiandra@gmail.com>
2023-04-01 16:41:45 +0200

    app3 connected with app4 and app5

--- app3/app/service/TraceService.scala
+++ app3/app/service/TraceService.scala
@@ -9,13 +9,24 @@ import scala.concurrent.{ExecutionContext, Future}
 
 class TraceService(ws: WSClient) extends Logging {
 
-  private lazy val apiURL = "http://app4:9004/api/v1/trace"
+  private lazy val api4URL = "http://app4:9004/api/v1/trace"
+  private lazy val api5URL = "http://app5:9005/api/v1/trace"
 
   def getTrace(ctxId: String)(implicit
       mc: MarkerContext,
       ex: ExecutionContext
   ): Future[TraceResponse] = {
-    ws.url(apiURL)
+    for {
+      _ <- getTrace(ctxId, api4URL)
+      result <- getTrace(ctxId, api5URL)
+    } yield result
+  }
+
+  private def getTrace(ctxId: String, url: String)(implicit
+      mc: MarkerContext,
+      ex: ExecutionContext
+  ): Future[TraceResponse] = {
+    ws.url(url)
       .withHttpHeaders(headers = (ContextId.cId, ctxId))
       .get()
       .map(response => {
