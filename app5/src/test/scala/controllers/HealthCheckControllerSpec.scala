ca09a0020bb500ab8bb349b80fd496053eb2ebea
Biandratti <maxibiandra@gmail.com>
2024-05-19 19:21:33 +0200

    zio update

--- app5/src/test/scala/controllers/HealthCheckControllerSpec.scala
+++ app5/src/test/scala/controllers/HealthCheckControllerSpec.scala
@@ -4,20 +4,21 @@ import zhttp.http._
 import zio.test.*
 
 object HealthCheckControllerSpec extends ZIOSpecDefault {
-  def spec: Spec[Any, Option[Nothing]] =
+  def spec: Spec[Any, Serializable] =
     suite("HealthCheckControllerSpec")(
       test("ok status") {
-        HealthCheckController
-          .routes(
-            Request(method = Method.GET, url = URL(!! / "health"))
-          )
-          .map(result =>
-            assertTrue(
-              result.data == Response
-                .html("<h1>Welcome</h1><p>APP5 is ready.</p>")
-                .data
+        for {
+          response <- HealthCheckController
+            .routes(
+              Request(method = Method.GET, url = URL(!! / "health"))
             )
+          body <- response.body.asString
+        } yield {
+          assertTrue(
+            body == "<!DOCTYPE html><h1>Welcome</h1><p>APP5 is ready.</p>",
+            response.status == Status.Ok
           )
+        }
       }
     )
 }
