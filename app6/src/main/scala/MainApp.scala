699fc07a2bde3ba0ab0ec8c846b0d1e0c98564e9
biandratti <maximiliano.biandratti@riskident.com>
2024-06-21 13:52:23 +0200

    otel as resource

--- app6/src/main/scala/MainApp.scala
+++ app6/src/main/scala/MainApp.scala
@@ -1,27 +1,20 @@
 import cats.effect.*
 import controllers.HealthCheckController
-import io.opentelemetry.api.GlobalOpenTelemetry
 import org.http4s.blaze.server.BlazeServerBuilder
 import org.http4s.implicits.*
-import org.typelevel.otel4s.Otel4s
-import org.typelevel.otel4s.oteljava.OtelJava
+import utils.OtelResource
 
 object MainApp extends IOApp {
 
-  private def otelResource[F[_]: Async: LiftIO]: Resource[F, Otel4s[F]] = {
-    Resource
-      .eval(Async[F].delay(GlobalOpenTelemetry.get))
-      .evalMap(OtelJava.forAsync[F])
-  }
-
   override def run(args: List[String]): IO[ExitCode] = {
-    otelResource[IO].use { _ =>
-      val serverResource = BlazeServerBuilder[IO]
+    val serverResource: Resource[IO, Unit] = for {
+      _ <- OtelResource.apply[IO]
+      _ <- BlazeServerBuilder[IO]
         .bindHttp(9006, "0.0.0.0")
         .withHttpApp(HealthCheckController.routes.orNotFound)
         .resource
+    } yield ()
 
-      serverResource.use(_ => IO.never).as(ExitCode.Success)
-    }
+    serverResource.use(_ => IO.never).as(ExitCode.Success)
   }
 }
---
+++ app6/src/main/scala/utils/OtelResource.scala
@@ -0,0 +1,14 @@
+package utils
+
+import cats.effect.{Async, LiftIO, Resource}
+import io.opentelemetry.api.GlobalOpenTelemetry
+import org.typelevel.otel4s.Otel4s
+import org.typelevel.otel4s.oteljava.OtelJava
+
+object OtelResource {
+  def apply[F[_] : Async : LiftIO]: Resource[F, Otel4s[F]] = {
+    Resource
+      .eval(Async[F].delay(GlobalOpenTelemetry.get))
+      .evalMap(OtelJava.forAsync[F])
+  }
+}
