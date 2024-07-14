f3510beae7ce4e4536aa3f7c1d5710c430310a00
Biandratti <maxibiandra@gmail.com>
2024-07-14 10:27:08 +0200

    context added to app controller

--- app6/src/main/scala/controllers/AppController.scala
+++ app6/src/main/scala/controllers/AppController.scala
@@ -4,14 +4,16 @@ import cats.effect.Async
 import cats.implicits.*
 import io.circe.generic.auto.*
 import io.circe.syntax.*
+import io.opentelemetry.api.trace.{Span, StatusCode}
 import org.http4s.*
-import org.http4s.implicits.*
 import org.http4s.circe.*
 import org.http4s.dsl.Http4sDsl
+import org.http4s.implicits.*
 import org.log4s.getLogger
 import org.typelevel.ci.CIString
 import org.typelevel.otel4s.trace.Tracer
-import utils.ServerMiddleware._
+import utils.ContextId
+import utils.ServerMiddleware.*
 
 case class TraceResponse(traceId: String)
 
@@ -20,14 +22,26 @@ object TraceResponse {
     jsonEncoderOf[F, TraceResponse]
 }
 
-class AppController[F[_]: Async](tracer: Tracer[F]) extends Http4sDsl[F] {
+class AppController[F[_]: Async](tracer: Tracer[F])
+    extends Http4sDsl[F]
+    with ContextId {
   implicit val t: Tracer[F] = tracer
   private[this] val logger = getLogger
 
   def trace: HttpRoutes[F] = HttpRoutes.of[F] {
-    case GET -> Root / "api" / "v1" / "trace" =>
+    case request @ GET -> Root / "api" / "v1" / "trace" =>
       logger.info("trace request...")
-      Ok(TraceResponse("WIP traceId").asJson)
+      Ok(TraceResponse(getSpan(request.headers).getSpanContext.getTraceId).asJson)
+  }
+
+  private def getSpan(requestHeader: Headers): Span = {
+    val span = Span.current()
+    if (getCtxId(requestHeader).equalsIgnoreCase("error")) {
+      logger.error("simulating an unexpected error")
+      span.setStatus(StatusCode.ERROR, "Unexpected error")
+      // span.end()
+    }
+    span
   }
 
   def index: HttpRoutes[F] = HttpRoutes.of[F] { case GET -> Root / "health" =>
